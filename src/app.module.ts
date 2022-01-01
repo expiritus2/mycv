4c3dec3c09e6bf0a569d017a6880048117b6ff56
Mikhail <michaildrazdovich@gmail.com>
2022-01-01 23:52:41 +0300

    Add middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,12 +28,6 @@ const cookieSession = require('cookie-session');
         };
       },
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
     UsersModule,
     ReportsModule,
   ],
